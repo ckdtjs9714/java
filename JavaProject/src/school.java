fc7dcf967bf9f4a4c02fb8d41ad8a058a9120236
USER <USER@INET-29>
2018-03-13 19:42:02 +0900

    20180313

---
+++ JavaProject/src/school.java
@@ -0,0 +1,42 @@
+
+public class school { 
+	String height;
+	String area;
+	int studentCount;
+	int teacherCoutnt;
+	void print(){
+		System.out.println
+		("학생"+studentCount+
+				"명과 선생님"+teacherCoutnt+
+				"명이 있는 학교는"+area+
+				"제곱센티미터의 부지에");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+class student{
+	String Department/*학과*/;
+	String grade;
+	String name;
+	String rank;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class teacher{
+	String subject;/*담당과목*/
+	String workroom;
+	String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
